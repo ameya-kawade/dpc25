bfdbe48d46fdb78eee97fca2d08f077fb8e6d48a
ameya-kawade <ameyakawade0@gmail.com>
2025-08-17 17:20:00 +0530

    day3 added bruteForce and better approaches

--- day3.cpp
+++ day3.cpp
@@ -1,25 +1,46 @@
-#include<iostream>
-#include<vector>
-#include<unordered_set>
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
-void printVector(vector<int> &v){
-    cout<<"[ ";
-    for(auto el : v){
+void printVector(vector<int> &v)
+{
+    cout << "[ ";
+    for (auto el : v)
+    {
         cout << el << " ";
     }
-    cout<<"]";
-    cout<<endl;
+    cout << "]";
+    cout << endl;
 }
 
+int bruteForceApproach(vector<int> v)
+{
+    sort(v.begin(), v.end());
+    int duplicateElement = -1;
 
-int bruteForceApproach(vector<int> &v){
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        if (v[i] == v[i + 1])
+        {
+            duplicateElement = v[i];
+            break;
+        }
+    }
+    return duplicateElement;
+}
+
+int betterApproach(vector<int> &v)
+{
     unordered_set<int> s;
     int duplicateElement = -1;
 
-    for(auto el : v){
-        if(s.find(el) != s.end()){
+    for (auto el : v)
+    {
+        if (s.find(el) != s.end())
+        {
             duplicateElement = el;
             break;
         }
@@ -29,8 +50,8 @@ int bruteForceApproach(vector<int> &v){
     return duplicateElement;
 }
 
-
-int optimizedApproach(vector<int> &v){
+int optimizedApproach(vector<int> &v)
+{
 
     // Using Floyd's Cycle dectection algorithm
     // Finding the begining of the cycle which helps us in finding duplicate element as duplicate element point to a same index thus forming a cycle
@@ -38,21 +59,25 @@ int optimizedApproach(vector<int> &v){
     int slow = 0;
     int fast = 0;
 
-    while(true){
+    while (true)
+    {
         slow = v[slow];
         fast = v[v[fast]];
 
-        if (slow == fast){
+        if (slow == fast)
+        {
             break;
         }
     }
 
     int secondSlow = 0;
-    
-    while(true){
+
+    while (true)
+    {
         secondSlow = v[secondSlow];
         slow = v[slow];
-        if(secondSlow == slow){
+        if (secondSlow == slow)
+        {
             break;
         }
     }
@@ -60,42 +85,63 @@ int optimizedApproach(vector<int> &v){
     return slow;
 }
 
-
-int main(){
+int main()
+{
     vector<vector<int>> v = {
         vector<int>({1, 3, 4, 2, 2}),
-        vector<int>( {3, 1, 3, 4, 2}),
+        vector<int>({3, 1, 3, 4, 2}),
         vector<int>({1, 1}),
-        vector<int>({1, 4, 4, 2, 3})
-    };
+        vector<int>({1, 4, 4, 2, 3})};
 
     int testcaseNo = 1;
 
-    cout << "Brute force approach : \n";
+    cout << "Brute Force approach : \n";
 
-    for(auto vec: v){
+    for (auto vec : v)
+    {
 
         cout << "testcase : " << testcaseNo << endl;
         printVector(vec);
         cout << bruteForceApproach(vec);
 
-        cout<<"\n\n"<<endl;
+        cout << "\n\n"
+             << endl;
         testcaseNo++;
     }
 
     cout << "\n\n";
 
     testcaseNo = 1;
-    
+
+    cout << "Better approach : \n";
+
+    for (auto vec : v)
+    {
+
+        cout << "testcase : " << testcaseNo << endl;
+        printVector(vec);
+        cout << betterApproach(vec);
+
+        cout << "\n\n"
+             << endl;
+        testcaseNo++;
+    }
+
+    cout << "\n\n";
+
+    testcaseNo = 1;
+
     cout << "Optimized approach : \n";
 
-    for(auto vec: v){
+    for (auto vec : v)
+    {
 
         cout << "testcase : " << testcaseNo << endl;
         printVector(vec);
         cout << optimizedApproach(vec);
 
-        cout<<"\n\n"<<endl;
+        cout << "\n\n"
+             << endl;
         testcaseNo++;
     }
     return 0;
